561729afc500b897cd7adaae6c864453db4582de
Bong Gyoon Sin <tlsqhdrbs3@gmail.com>
2023-03-12 17:11:15 +0900

    update

--- python/programmers 파이썬 문제 푼 것/Testspace.py
+++ python/programmers 파이썬 문제 푼 것/Testspace.py
@@ -1,1 +1,7 @@
-print( [i for i in range(1,20)])
+data= dict(num=[1,2,3,4,5,6,7,8,9,0],
+               jisu=['S','D','T'],
+               event=['*','#'])
+
+dar= [-1,2,3,4,5]
+dar[0]*=3
+print(dar[0] == True)
