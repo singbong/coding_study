8ee1ebb798c57df7726f727efe93ed5c329f6e4b
Bong Gyoon Sin <tlsqhdrbs3@gmail.com>
2023-03-19 19:42:00 +0900

    update

--- python/programmers 파이썬 문제 푼 것/Testspace.py
+++ python/programmers 파이썬 문제 푼 것/Testspace.py
@@ -1,20 +1,13 @@
-def solution(X, Y):
-    x_set = set(X)
-    y_set = set(Y)
-    common_set = x_set & y_set
-    if not common_set: # X, Y에 공통된 숫자가 없을 경우
-        return "-1"
-    result = []
-
-    for i in common_set:
-        x_count = X.count(i)
-        y_count = Y.count(i)
-        pair_count = min(x_count, y_count)
-        result += [i] * pair_count
-    result.sort(reverse=True)
-    if result[0] == "0": # 결과가 0으로 시작하는 경우
-        return "0"
-    
-    return "".join(result)
-
-print(solution(	"100", "123450"))
+def solution(number, limit, power):
+    yaksu= list()
+    for i in range(1,number+1):
+        num=0
+        for v in range(1,i+1):
+            if i % v == 0:
+                num+=1
+            if num > limit:
+                num= power
+                break
+        yaksu.append(num)
+    return sum(yaksu)
+print(solution(10,3,2))
