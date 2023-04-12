0aa0161441d763871fdf275891098d9881fb3423
Bong Gyoon Sin <tlsqhdrbs3@gmail.com>
2023-04-12 20:43:39 +0900

    update

--- R/R 학교 수업/01.R
+++ R/R 학교 수업/01.R
@@ -1,3 +1,6 @@
-install.packages('mlbench')
-a= as.numeric('314.5')
-a
+idx= sample(1:nrow(iris), size= 50,
+            replace=FALSE)
+idx
+iris.50 = iris[idx,] #50개의 행 추출
+dim(iris.50)          #행과 열의 갯수 확인
+head(iris.50)
--- python/자료구조와 알고리즘/Testspace2.py
+++ python/자료구조와 알고리즘/Testspace2.py
@@ -1,75 +1,44 @@
-import random
-
-class Node():
-    def __intit__ (self):
-        self.data=None
-        self.link=None
-
+class Node2():
+    def __init__(self):
+        self.plink = None
+        self.data= None
+        self.nlink= None
 
 def printNodes(start):
     current= start
-    if current.link == None:
+    if current.nlink == None:
         return
+    print("정방향 -->", end=' ')
     print(current.data, end=' ')
-    while current.link != start:
-        current= current.link
+    while current.nlink != None :
+        current = current.nlink
         print(current.data, end=' ')
-        print()
-
-def countOddEven():
-    global memory, head, current, current
-
-    odd, even= 0, 0
-    if head == None:
-        return False
-    
-    current= head
-
-    while True:
-        if current.data % 2 ==0:
-            even +=1
-        else:
-            odd +=1
-        if current.link == head:
-            break;
-        current= current.link
-
-    return odd, even
-
-
-def makeZeroNumber(odd, even):
-    if odd> even:
-        reminder = 1
-    else:
-        reminder = 0
-    current=head
-    while True:
-        if current.data %2==0:
-            current.data *=-1
-        if current.link == head:
-            break
-        current= current.link
+    print()
+    print('역방향-->',end=' ')
+    print(current.data, end=' ')
+    while current.plink != None:
+        current= current.plink
+        print(current.data, end=' ')
 
 ##전역 변수 선언 부분##
-memory=[]
+
+memory= []
 head, current, pre= None, None, None
+dataArray= ['다현','정연','쯔위','사나','지효']
 
 ##메인 코드 부분##
 if __name__ == '__main__':
-    dataArray= []
-    for _ in range(7):
-        dataArray.append(random.randint(1,100))
-    print(dataArray)
-    
-    node = Node()
-    node.data= dataArray[0] 
+    node= Node2()
+    node.data= dataArray[0]
     head= node
-    node.link= head
     memory.append(node)
-    
-    oddCount, evenCount = countOddEven()
-    print('홀수---->',oddCount,'\t','짝수---->',evenCount)
-    
-    
-    makeZeroNumber(oddCount, evenCount)
+
+    for data in dataArray[1:]:
+        pre= node
+        node= Node2()
+        node.data= data
+        pre.nlink= node
+        node.plink= pre
+        memory.append(node)
+
     printNodes(head)
