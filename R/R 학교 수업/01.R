ad8433ae4e39a942c60ec131d23ec7ddacc3255a
Bong Gyoon Sin <tlsqhdrbs3@gmail.com>
2023-05-01 22:13:06 +0900

    update

--- R/R 학교 수업/01.R
+++ R/R 학교 수업/01.R
@@ -1,7 +1,6 @@
 library(ggmap)
 ##ggmap::register_google(key = '본인 API 키')##
-ggmap::register_google(key = 'AIzaSyDRI065JSqKKXqcmVbZQ7GiNnoKp0dA0ys')
-
+register_google(key = 'AIzaSyDRI065JSqKKXqcmVbZQ7GiNnoKp0dA0ys')
 cen = c(127.1568,37.8732)        #경도와 위도 값을 center값으로 직접 입력
 map <- get_googlemap(center=cen,
                      zoom = 20,
